e7425b16c2af4c372cdabe1c6fdbba0a29dbe148
sumire837 <mjxyh837@gmail.com>
2025-06-28 15:12:52 +0900

    profile add

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,24 @@
+class ProfilesController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
+  def edit; end
+
+  def update
+    if @user.update(user_params)
+      redirect_to profile_path, success: t('defaults.flash_message.updated', item: User.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :profile_image, :profile_image_cash, :age, :gender)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  resource :profile, only: %i[show edit update]
 end
