a83c93cb191e01f3b51575fc24f303e0f2a687d5
sumire837 <mjxyh837@gmail.com>
2025-06-30 21:37:21 +0900

    投稿機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
   config.enable_reloading = true
 
   # Do not eager load code on boot.
@@ -40,6 +41,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
