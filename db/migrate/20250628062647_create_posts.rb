0d4108c0b3c1f6ce5bfb7669a9f082796df7677a
sumire837 <mjxyh837@gmail.com>
2025-07-14 02:37:07 +0900

    マイグレーションファイル修正

--- db/migrate/20250628062647_create_posts.rb
+++ db/migrate/20250628062647_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.string :location, null: false
+      t.string :location
       t.string :access
       t.string :url
       t.text :content, null: false
