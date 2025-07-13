57177fba8b189f8efea79008963a53a1ea82a192
sumire837 <mjxyh837@gmail.com>
2025-07-14 02:42:49 +0900

    不要マイグレーションファイル削除

--- db/migrate/20250628062647_create_posts.rb
+++ db/migrate/20250628062647_create_posts.rb
@@ -2,13 +2,14 @@ class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
       t.string :title, null: false
-      t.string :location
+      t.text :location
+      t.integer :area, null: false
+      t.integer :category, null: false
       t.string :access
       t.string :url
       t.text :content, null: false
       t.string :post_image
       t.references :user, foreign_key: true
-
       t.timestamps
     end
   end
--- db/migrate/20250712035225_change_location_to_integer_and_add_category_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeLocationToIntegerAndAddCategoryToPosts < ActiveRecord::Migration[7.1]
-  def change
-    change_column :posts, :location, :integer, null: false
-    add_column :posts, :category, :integer, null: false
-  end
-end
