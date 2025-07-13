7f9cf5f2228ac0297634647ad6c2bcf0ca5d172f
sumire837 <mjxyh837@gmail.com>
2025-07-14 02:26:17 +0900

    不具合修正

--- db/migrate/20250712032320_add_details_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddDetailsToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :gender, :integer
-    add_column :users, :age, :integer
-    add_column :users, :comment, :text
-    add_column :users, :profile_image, :string
-  end
-end
