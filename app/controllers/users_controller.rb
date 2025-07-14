485bc3146fb26f61db5ad433de6314ed6ac92b90
sumire837 <mjxyh837@gmail.com>
2025-07-14 20:39:11 +0900

    登録失敗時のメッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, t('defaults.flash_message.created', item: User.model_name.human)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
