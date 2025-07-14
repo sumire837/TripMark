3cfaec5ac58ff138e23676bb8cf46f3e261ba244
sumire837 <mjxyh837@gmail.com>
2025-07-14 11:59:13 +0900

    画像修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,12 @@ class Post < ApplicationRecord
   enum category: {Shopping: 0, Fashion: 1, Food: 2, Drink: 3, Sightseeing: 4, Amusement: 5, Art: 6, Hotspring: 7, Hotel: 8, BeautifulView: 9, category_other: 10 }
   enum area: {Hokkaido: 0, Tohoku: 1, Kanto: 2, Syutoken: 3, Hokuriku: 4, Tyubu: 5, Kinki: 6, Sikoku: 7, Tyugokutihou: 8, Kyusyu: 9, Asea: 10, Europe: 11, Africa: 12, NorthAmerica: 13, SouthAmerica: 14, area_other:15 }
   belongs_to :user
+
+  def post_image_url
+    if post_image.present?
+      post_image
+    else
+      "sample.jpg"
+    end
+  end
 end
